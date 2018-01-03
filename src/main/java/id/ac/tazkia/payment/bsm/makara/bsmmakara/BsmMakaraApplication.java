1a442019699842b4d2fc36188f554096189283a3
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-03 11:57:32 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/payment/bsm/makara/bsmmakara/BsmMakaraApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.payment.bsm.makara.bsmmakara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmMakaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmMakaraApplication.class, args);
+	}
+}
