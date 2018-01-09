e4803d8a3e1ecdeaa6b8f7d2ffe5a861ab7ba0f5
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-09 20:14:20 +0700

    enable debug

--- src/main/java/id/ac/tazkia/payment/bsm/makara/bsmmakara/BsmMakaraApplication.java
+++ src/main/java/id/ac/tazkia/payment/bsm/makara/bsmmakara/BsmMakaraApplication.java
@@ -2,9 +2,21 @@ package id.ac.tazkia.payment.bsm.makara.bsmmakara;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class BsmMakaraApplication {
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(BsmMakaraApplication.class, args);
