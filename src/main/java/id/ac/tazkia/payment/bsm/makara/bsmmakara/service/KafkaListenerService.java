d99eb7b65be3acf6f1508d60b63b13ac56dee813
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-15 00:20:48 +0700

    sync pembayaran dihandle aplikasi-tagihan saja

--- src/main/java/id/ac/tazkia/payment/bsm/makara/bsmmakara/service/KafkaListenerService.java
+++ src/main/java/id/ac/tazkia/payment/bsm/makara/bsmmakara/service/KafkaListenerService.java
@@ -3,10 +3,7 @@ package id.ac.tazkia.payment.bsm.makara.bsmmakara.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import id.ac.tazkia.payment.bsm.makara.bsmmakara.dao.VirtualAccountDao;
 import id.ac.tazkia.payment.bsm.makara.bsmmakara.dto.RequestType;
-import id.ac.tazkia.payment.bsm.makara.bsmmakara.dto.VaPayment;
 import id.ac.tazkia.payment.bsm.makara.bsmmakara.dto.VirtualAccountRequest;
-import id.ac.tazkia.payment.bsm.makara.bsmmakara.entity.AccountStatus;
-import id.ac.tazkia.payment.bsm.makara.bsmmakara.entity.VirtualAccount;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,59 +43,6 @@ public class KafkaListenerService {
                 LOGGER.warn("Virtual Account Request Type {} belum dibuat", vaRequest.getRequestType());
             }
         } catch (Exception err){
-            LOGGER.error(err.getMessage(), err);
-        }
-    }
-
-    @KafkaListener(topics = "${kafka.topic.va.payment}", groupId = "${spring.kafka.consumer.group-id}")
-    public void handleVaPayment(String message) {
-        try {
-            LOGGER.debug("Receive message : {}", message);
-            VaPayment payment = objectMapper.readValue(message, VaPayment.class);
-
-            // notif dari diri sendiri, tidak perlu diproses
-            if (bankId.equalsIgnoreCase(payment.getBankId())) {
-                return;
-            }
-
-            VirtualAccount va = virtualAccountDao.findByInvoiceNumberAndAccountStatusIn(payment.getInvoiceNumber(), AccountStatus.UNPAID, AccountStatus.PAID_PARTIALLY);
-            // bila va tidak terdaftar di sini, tidak perlu dilanjutkan
-            if (va == null) {
-                return;
-            }
-
-            LOGGER.info("VA {} dibayar di bank {} senilai {}",
-                    va.getAccountNumber(),
-                    payment.getBankId(),
-                    payment.getCumulativeAmount());
-
-            // create lagi VA dengan nilai baru
-            VirtualAccountRequest request = VirtualAccountRequest.builder()
-                    .requestTime(LocalDateTime.now())
-                    .requestType(RequestType.CREATE)
-                    .accountNumber(va.getAccountNumber())
-                    .accountType(va.getAccountType())
-                    .bankId(bankId)
-                    .description(va.getDescription())
-                    .email(va.getEmail())
-                    .invoiceNumber(va.getInvoiceNumber())
-                    .expireDate(va.getExpireDate())
-                    .name(va.getName())
-                    .phone(va.getPhone())
-                    .build();
-
-            request.setAmount(va.getAmount().subtract(payment.getCumulativeAmount()));
-
-            // kalau pembayaran full, hapus va
-            if (va.getAmount().compareTo(payment.getCumulativeAmount()) == 0) {
-                request.setRequestType(RequestType.DELETE);
-                virtualAccountService.deleteVirtualAccount(request);
-            } else {
-                request.setRequestType(RequestType.UPDATE);
-                virtualAccountService.updateVirtualAccount(request);
-            }
-
-        } catch (Exception err) {
             LOGGER.error(err.getMessage(), err);
         }
     }
